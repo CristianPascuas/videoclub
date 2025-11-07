d8aacbc94c8d8b3acf45cb09227b41766e2ba1d0
Cristian <herneycristian@gmail.com>
2025-11-07 11:47:47 -0500

    commit9

--- include/conex.php
+++ include/conex.php
@@ -7,7 +7,7 @@ class Connection {
         $servername = "localhost";
         $db = "videoclub";
         $username = "sena123";
-        $password = "sena123";
+        $password = $_ENV["DB_PASSWORD"];
         $conEctar = mysqli_connect($servername, $username, $password, $db);
         if (!$conEctar) {
             die("Error de Conexion: " . mysqli_connect_error());
